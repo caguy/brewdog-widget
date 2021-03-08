7b88a0d5c8325704884d72cf0fa01b796977ff10
caguy <camille.guy@gmail.com>
2021-03-08 15:53:23 +0100

    Added component lifecycle and basic fetch to a random beer

--- src/components/brewdog-widget/brewdog-widget.tsx
+++ src/components/brewdog-widget/brewdog-widget.tsx
@@ -1,4 +1,5 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, State } from '@stencil/core';
+import axios from 'axios';
 
 @Component({
   tag: 'brewdog-widget',
@@ -6,7 +7,35 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class BrewdogWidget {
+  @State() status: 'loading' | 'loaded' | 'error';
+  @State() beers: any[];
+
+  async fetchBeers() {
+    try {
+      const beers = await axios.get('https://api.punkapi.com/v2/beers/random');
+      this.status = 'loaded';
+      this.beers = beers.data;
+    } catch (err) {
+      this.status = 'error';
+    }
+  }
+
+  connectedCallback() {
+    this.status = 'loading';
+  }
+
+  componentWillLoad() {
+    this.fetchBeers();
+  }
+
   render() {
-    return <Host></Host>;
+    return (
+      <Host>
+        <h1>Random beer :</h1>
+        {this.status === 'loading' && <span>Loading...</span>}
+        {this.status === 'loaded' && <span>{this.beers[0]?.name}</span>}
+        {this.status === 'error' && <span>An error has occured</span>}
+      </Host>
+    );
   }
 }
