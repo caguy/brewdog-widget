2cc969283ee543183767b92ddccfbbe8af3aee65
caguy <camille.guy@gmail.com>
2021-04-06 11:22:55 +0200

    Updates

---
+++ build/brewdog-widget.esm.js
@@ -0,0 +1,1 @@
+import{p as e,b as o}from"./p-f91511ec.js";(()=>{const o=import.meta.url,r={};return""!==o&&(r.resourcesUrl=new URL(".",o).href),e(r)})().then((e=>o([["p-c7cd84d2",[[1,"brewdog-widget",{food:[1],status:[32],beerPool:[32],currentIndex:[32]}]]]],e)));
